3f1422a0a8bb121c8d64fb7f32169673b2691064
Ghaithu <30323454+Ghaithu@users.noreply.github.com>
2018-12-20 23:44:23 +0100

    first init with this game

---
+++ MovingSprites/Game1.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using MovingSprites.Models;
+using MovingSprites.Sprites;
+
+namespace MovingSprites
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private List<Sprite> _sprites;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // NOTE: I no-longer use this reference as it affects different objects if being used multiple times!
+            var animations = new Dictionary<string, Animation>()
+      {
+        
+        { "WalkLeft", new Animation(Content.Load<Texture2D>("Player/WalkingLeft"), 5) },
+        { "WalkRight", new Animation(Content.Load<Texture2D>("Player/WalkingRight"), 5) },
+      };
+
+            _sprites = new List<Sprite>()
+      {
+        new Sprite(new Dictionary<string, Animation>()
+        {
+
+          { "WalkLeft", new Animation(Content.Load<Texture2D>("Player/WalkingLeft"), 5) },
+          { "WalkRight", new Animation(Content.Load<Texture2D>("Player/WalkingRight"), 5) },
+           
+        }
+        )
+        {
+          Position = new Vector2(50,350),
+          Input = new Input()
+          {
+            Left = Keys.A,
+            Right = Keys.D,
+            Space = Keys.Space,
+          },
+        },
+
+      };
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            foreach (var sprite in _sprites)
+                sprite.Update(gameTime, _sprites);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            foreach (var sprite in _sprites)
+                sprite.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MovingSprites/Sprites/Sprite.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovingSprites.Managers;
+using MovingSprites.Models;
+
+namespace MovingSprites.Sprites
+{
+    public class Sprite
+    {
+        #region Fields
+
+        protected AnimationManager _animationManager;
+
+        protected Dictionary<string, Animation> _animations;
+
+        protected Vector2 _position;
+
+        protected Texture2D _texture;
+
+        protected bool hasJumped = false;
+
+        #endregion
+
+        #region Properties
+
+        public Input Input;
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+
+                if (_animationManager != null)
+                    _animationManager.Position = _position;
+            }
+        }
+
+        public float Speed = 1f;
+
+        public Vector2 Velocity;
+
+       
+
+        #endregion
+
+        #region Methods
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (_texture != null)
+                spriteBatch.Draw(_texture, Position, Color.White);
+            else if (_animationManager != null)
+                _animationManager.Draw(spriteBatch);
+            else throw new Exception("This ain't right..!");
+        }
+
+          public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public virtual void Move()
+        {
+            Speed = 4;
+            if (Keyboard.GetState().IsKeyDown(Input.Up))
+                Velocity.Y = -Speed;
+            else if (Keyboard.GetState().IsKeyDown(Input.Down))
+                Velocity.Y = Speed;
+            else if (Keyboard.GetState().IsKeyDown(Input.Left))
+                Velocity.X = -Speed;
+            else if (Keyboard.GetState().IsKeyDown(Input.Right))
+                Velocity.X = Speed;
+        }
+
+        // float Bleedoff = 1.0f;
+        //readonly Vector2 gravity = new Vector2(0, -9.8f);
+
+        public virtual void Jump()
+        {
+            //float time = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (Keyboard.GetState().IsKeyDown(Input.Space) && hasJumped == false)
+            {
+                _position.Y -= 55f;
+                Velocity.Y = -10;
+                
+                hasJumped = true;
+
+                
+
+            }
+
+            if (hasJumped == true)
+            {
+               
+               float i = 1f;
+                Velocity.Y = -5f;
+                if(_position.Y >= 500)
+                {
+                    Velocity.Y = 1000;
+                }
+
+            }
+
+            if(_position.Y >= 350)
+            {
+                hasJumped = false;
+            }
+
+            if (hasJumped == false)
+            {
+                Velocity.Y = 0f;
+            }
+        }
+
+        protected virtual void SetAnimations()
+        {
+            if (Velocity.X > 0)
+                _animationManager.Play(_animations["WalkRight"]);
+            else if (Velocity.X < 0)
+                _animationManager.Play(_animations["WalkLeft"]);
+                
+           
+            else _animationManager.Stop();
+        }
+
+        public Sprite(Dictionary<string, Animation> animations)
+        {
+            _animations = animations;
+            _animationManager = new AnimationManager(_animations.First().Value);
+        }
+
+      
+
+        public virtual void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            
+            Move();
+            Jump();
+
+            SetAnimations();
+
+            _animationManager.Update(gameTime);
+
+            Position += Velocity;
+            Velocity = Vector2.Zero;
+        }
+
+        #endregion
+    }
+}
